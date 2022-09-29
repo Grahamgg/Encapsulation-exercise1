41f9c37e7649dee9ceeda8678570e6d6c9beba4e
Graham Galloway <Grahamgalloway0@gmail.com>
2022-09-29 14:03:41 -0400

    initial commit

---
+++ BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    internal class BankAccount
+    {
+        private double balance = 0;
+
+        public BankAccount()
+        {
+
+        }
+        public void Deposit(double depositAmount)
+        {
+            balance += depositAmount;
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+        
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var acc = new BankAccount();
+            var bal = acc.GetBalance();
+            
+
+            Console.WriteLine("How much do you want to deposit");
+            var depositAmount = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine($"Your account balance is now {bal + depositAmount}");
+            
+
+        }
+    }
+}
